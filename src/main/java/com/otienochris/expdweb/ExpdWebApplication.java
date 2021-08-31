fa30673a9fea66cfbf4530e149e168249e32caf8
christopherochiengotieno <christopherochiengotieno@gmail.com>
2021-08-31 08:58:11 +0300

    first commit

---
+++ src/main/java/com/otienochris/expdweb/ExpdWebApplication.java
@@ -0,0 +1,13 @@
+package com.otienochris.expdweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpdWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpdWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otienochris/expdweb/ExpdWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otienochris.expdweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpdWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
